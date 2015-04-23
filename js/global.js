af9c2d859360f6035fda226794ee5d118b940c58
denk0 <denko1024@gmail.com>
2015-04-23 15:22:58 -0700

    fixed-headers

---
+++ js/global.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$('.go-next').click(function() {
+		if ($('body').hasClass('fp-viewing-premium')) {
+			var nextLink = '#about-site';
+		} else{
+			var nextLink = $('#fp-nav li a.active').parent().next().children('a').attr('href');
+		};
+		$(".go-next").attr("href", nextLink);
+
+		$('.overlay').fadeOut(700);
+		$('.form').fadeOut(700);
+	});
+	$('.branding-pages__box').click(function(){
+		$('.branding-pages__box').removeClass('index-more');
+		$(this).addClass('index-more');
+	});
+	$('.order').click(function() {
+		$('.overlay').fadeIn(700);
+		$('.form').fadeIn(700);
+	});
+	$('.overlay, #fp-nav').click(function() {
+		$('.overlay').fadeOut(700);
+		$('.form').fadeOut(700);
+	});
+
+	$('#fp-nav li a[href="#about-site-2"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#advertising-block"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#top-phrases"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#announcement"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#geotagging"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#site-production"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#delete-competitors"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#video-production"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#slideshow"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#locate-video"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#up-feedback"]').parent().addClass('menu-none');
+	$('#fp-nav li a[href="#url"]').parent().addClass('menu-none');
+
+});
